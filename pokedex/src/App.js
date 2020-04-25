07156729e9130f2b2a232ac87862945182023800
David Stanley <davidmstanleyjr@gmail.com>
2020-04-25 19:48:09 -0400

    created some folders and a few files. Started working on the navbar

---
+++ pokedex/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import './App.css';
+import NavBar from "./components/layout/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
